86a7d6bf64e2dfe94883448db775460b606a1343
CeCamillo <cesarcamillo2212@gmail.com>
2022-08-08 19:02:11 -0300

    Pratical Test

---
+++ src/main/java/test.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+//        Ignoring every blank space in the string
+        input = input.replace(" ", "");
+//        Ignoring every numeric value in the string
+        input = input.replaceAll("[0-9]", "");
+
+        System.out.printf("Original String: %s%n", input);
+
+        char[] chars = input.toCharArray();
+
+        for (int i = 0; i < input.length(); i++) {
+            System.out.println(input.charAt(i) + " : " + input.chars().count());
+        }
+    }
+}
